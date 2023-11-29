dfd03b3f84286c6d1b4e549579c36c5b41b7b6c3
namhihi237 <yentth237@gmail.com>
2023-11-29 23:39:36 +0700

    feat: ui navbar

---
+++ frontend/src/components/navbar/Logo.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import { ROUTE } from '../../configs/route';
+
+export default function Logo() {
+	return (
+		<Link href={ROUTE.HOME}>
+			<strong className="text-cyan-700 text-2xl">AI GALLERY</strong>
+		</Link>
+	);
+}
---
+++ frontend/src/components/search/SearchBox.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useState } from 'react';
+import { Icon } from '../Icon';
+
+export default function SearchBox(): JSX.Element {
+	return (
+		<div className="bg-black w-2/5 rounded-xl h-10 flex px-6 items-center border-solid border-[1px] border-zinc-600">
+			<Icon iconName="GrSearch" propsIcon={{ size: 20 }} />
+			<input className="bg-black focus:outline-none ml-2 h-8 w-full" type="text" />
+		</div>
+	);
+}
